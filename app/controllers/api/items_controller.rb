56b28f19a05bae7e9e9b4cca9858194bc40eefd6
Qi Cao <alanqicao@gmail.com>
2020-04-14 22:29:26 -0600

    sort work

--- app/controllers/api/items_controller.rb
+++ app/controllers/api/items_controller.rb
@@ -1,4 +1,27 @@
 class Api::ItemsController < ApplicationController
+
+
   def index
+    render json: Item.all
   end
+
+  def create
+    item = Item.new(item_params)
+
+    if item.save
+      render json: item
+    else
+      render json:{message:'sorry not working'}
+    end
+
+  end
+
+  private
+
+
+  def item_params
+    params.require(:item).permit(:name,:image,:description,:likes)
+  end
+
+
 end
--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,96 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from "axios"
+import { Card,Image,Icon,Button } from "semantic-ui-react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App = ()=> {
+
+  const [items,setItems] = useState([]);
+  const [sort, setSort] = useState(null)
+ 
+
+useEffect(()=>{
+  axios
+  .get("/api/items")
+  .then(res=>{
+    setItems(res.data)
+  })
+  .catch((err)=>{
+    console.log(err)
+  })
+},[])
+
+const sortLikebymost=()=>{
+ 
+   const  most = items.sort(function(a,b){return b.likes-a.likes});
+     setItems(most,setSort(false)); 
+       
 }
+
+const sortLikebyleast=()=>{
+ 
+  const  least = items.sort(function(a,b){return a.likes-b.likes}); 
+    setItems(least,setSort(true));
+      
+}
+
+
+
+
+
+
+const renderItems=()=>{
+  return items.map(item =>(
+    
+    <Card key = {`ite-${item.id}`} style={{ margin: "0em 1em" }} >
+    <Image src= {item.image} wrapped ui={false} />
+    <Card.Content>
+      <Card.Header>{item.name}</Card.Header>
+      <Card.Meta>
+        {/* <span className='date'>Joined in 2015</span> */}
+      </Card.Meta>
+      <Card.Description>
+        {item.description}
+      </Card.Description>
+    </Card.Content>
+    <Card.Content extra>
+      <a>
+        <Icon name='user' />
+       likes: {item.likes}
+      </a>
+    </Card.Content>
+  </Card>
+  
+
+   
+  ))
+}
+
+  return (
+    <>
+      <Button primary onClick ={sortLikebymost} >Sort by most</Button>
+    <br/>
+      <Button primary onClick ={sortLikebyleast} >Sort by least</Button>
+    <br/>
+    
+<Card.Group   >
+   {sort? renderItems(): renderItems()}
+   </Card.Group>
+   </>
+  )
+}
+
+// const RowCenter = styled.div`
+
+// `
+// t.string "name"
+// t.string "image"
+// t.text "description"
+// t.integer "likes"
+
+
+
+
+
 
 export default App;
